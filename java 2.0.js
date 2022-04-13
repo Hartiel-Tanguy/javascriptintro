e2096f634ccf181a511363fee55914de957c87d4
Antoine <tanguy.hartiel.1@gmail.com>
2022-04-13 14:05:37 +0200

    on avance

--- java 2.0.js
+++ java 2.0.js
@@ -13,3 +13,71 @@ if ((min<current) && (max>current)) {
 } else {
     console.log("réfléchissé mieu");
 }
+
+
+let nem = 0;
+while (nem <= 100) {
+    console.log(nem);
+    nem +=2
+}
+for (let nom = 0; nom <=100; nom +=2){
+    console.log(nom)
+}
+
+
+
+
+
+let num = prompt("entre ton num favorie")
+ 
+if (num == "42") {
+        alert("voila un beau chifre")
+} else if (num != "42") { while (num != "42") {
+        alert("pas ouf trouve en un autre");
+        num = prompt("alors le quel chooissi tu");
+        }
+        if (num == "42")
+        {alert("voila beaucoups mieu se chiffe")}
+}
+
+
+
+
+
+
+
+
+let j = prompt("donner un chiffre entre 1 et 7")
+
+if (j=="1"){
+    console.log("lundi");
+} else if(j=="2"){
+    console.log("mardi")
+
+} else if (j=="3"){
+    console.log("mercredi")
+} else if(j=="4"){
+    console.log("jeudi")
+} else if(j=="5"){
+    console.log("vendredi")
+} else if(j=="6"){
+    console.log("samedi")
+} else if(j=="7"){
+    console.log("dimanche")
+}
+
+
+
+
+let n = Number(prompt("entré un nombre"));
+let fin = 0;
+let ha = 0;
+
+while (n!=0) {
+    ha = Number(prompt("Entre un numéro MAINTENANT"));
+    fin += ha;
+    n --
+}
+
+alert ("The result is  " + fin)
+
---
+++ java3.0.js
@@ -0,0 +1,38 @@
+let nom = ["hipster" , "gille" , "jason" , "jeremy" , "emre" , "rayane"];
+for (let elem of nom){
+    console.log("bonjour a "+elem+"?")
+}
+
+
+const array = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+console.log(sum);
+
+const array = [100, 101, 102];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+console.log(sum);
+
+
+
+
+
+arr=[1, 2, 3, 4, 5];
+let sum =0;
+for (let i =0;i<arr.length;i++){
+    sum +=parseInt(arr[i],10);
+}
+var avg = sum / arr.length;
+console.log(avg);
+
+
+
+
+
