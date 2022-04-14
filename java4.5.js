c47f1b6c3ab3062c7db9fba05ff21b7cad69a5ee
Antoine <tanguy.hartiel.1@gmail.com>
2022-04-14 12:17:03 +0200

    4.5?

---
+++ java4.5.js
@@ -0,0 +1,7 @@
+function calcDistance(a){
+    return a*a;
+} 
+function Distance(x1, y1, x2, y2) {
+    return Math.sqrt(calcalcDistance(y2 - y1) + calcDistance(x2 - x1));
+} 
+console.log(+Distance(4, 1, 1, 1));
